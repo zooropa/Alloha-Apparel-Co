2f228fe7258951218b835431f0115aee58190d89
zooropa <yosrismail@gmail.com>
2017-07-17 16:47:59 -0400

    CHANGES
    UPDATE

---
+++ script3.js
@@ -0,0 +1,16 @@
+/*var b =document.getElementByID("CAT");
+b. onclick (fuction () {
+	alert ("hi");
+	}); */
+
+$(".subscribe").on("click", function () {
+
+$(this).fadeOut();
+	
+/*alert ("Thank You");*/
+});
+
+$('.bxslider').bxSlider({
+  infiniteLoop: false,
+  hideControlOnEnd: true
+});
